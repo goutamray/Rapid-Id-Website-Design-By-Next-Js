4d0d533b5b797ba35e67b2a01a10d39ff17544e4
goutam <goutamr362@gmail.com>
2024-05-14 10:05:20 +0600

    login & regisater & forget password done

--- app/(auth)/forget-password/page.jsx
+++ app/(auth)/forget-password/page.jsx
@@ -2,33 +2,29 @@
 import logo from "@/public/logo.svg"
 import Image from "next/image"
 import Link from "next/link"
-import styles from "./login.module.css"
-export default function Login() {
+
+import styles from "./forget.module.css"; 
+export default function Forget() {
   return (
     <>
-      <div className={styles.login}>  
+      <div className={styles.forget}>  
         <div className="container">
-          <div className={styles.login_form}>
+          <div className={styles.forget_form}>
               <div className={styles.logo}>
                   <Image src={logo} alt="logo"/> 
               </div>
               <div className={styles.form_data}>
-                <h3> Sign In </h3>
                 <form action="">
                   <div className={styles.my_2}>
                     <label htmlFor="email"> Email </label>
                     <input type="email" id="email"/>
                   </div>
                   <div className={styles.my_2}>
-                    <label htmlFor="pass"> Password </label>
-                    <input type="text" id="pass"/>
+                    <Link href="/login"> I remembered my password </Link>
                   </div>
-                  <div className={styles.my_2} style={{display : "flex", justifyContent : "space-between"}}>
-                    <Link href="/register"> Create Account </Link>
-                    <Link href="/forget-password"> Forget Password </Link>
-                  </div>
-                  <button> Log In </button>
+                  <button> Send forgot password email </button>
                 </form>
+
               </div>
           </div>
         </div>
@@ -36,4 +32,6 @@ export default function Login() {
     </>
   )
 }
+
+
 
--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -3,26 +3,29 @@ import logo from "@/public/logo.svg"
 
 import styles from "./register.module.css"
 import Image from "next/image"
+import Link from "next/link"
 export default function Register() {
   return (
     <>
       <div className={styles.signUp}>  
         <div className="container">
           <div className={styles.signUp_form}>
-              <div className="logo">
+              <div className={styles.logo}>
                   <Image src={logo} alt="logo"/> 
               </div>
               <div className={styles.form_data}>
                 <h3> Create an Account </h3>
                 <form action="">
                   <div className={styles.my_2}>
-                    <div className={styles.first_name}>
-                      <label htmlFor="fastName"> First Name </label>
-                      <input type="text" id="fastName" />
-                    </div>
-                    <div className={styles.last_name}>
-                    <label htmlFor="lastName"> Last Name </label>
-                      <input type="text" id="lastName" />
+                    <div className={styles.name_box}> 
+                      <div className={styles.first_name}>
+                        <label htmlFor="fastName"> First Name </label>
+                        <input type="text" id="fastName" />
+                      </div>
+                      <div className={styles.last_name}>
+                      <label htmlFor="lastName"> Last Name </label>
+                        <input type="text" id="lastName" />
+                      </div>
                     </div>
                   </div>
                   <div className={styles.my_2}>
@@ -32,6 +35,10 @@ export default function Register() {
                   <div className={styles.my_2}>
                     <label htmlFor="number"> Phone Number </label>
                     <input type="text" id="number"/>
+                  </div>
+                  <div className={styles.my_2}>
+                    <label htmlFor="pass"> Password </label>
+                    <input type="text" id="pass"/>
                   </div>
                   <div className={styles.my_2}>
                     <label htmlFor="company"> Company Name (legal entity) </label>
@@ -50,6 +57,9 @@ export default function Register() {
                   </div>
                   <button> Create Account </button>
                 </form>
+                <div className={styles.my_2}>
+                    <Link href="/login"> Already have an account </Link>
+                </div>
               </div>
           </div>
         </div>
