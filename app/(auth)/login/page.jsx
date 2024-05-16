1b94e0126969843a6cdd09a02e7aa75f3065c5f7
goutam <goutamr362@gmail.com>
2024-05-16 20:19:50 +0600

    register & login page updated

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,9 +1,33 @@
+"use client"
 
 import logo from "@/public/logo.svg"
 import Image from "next/image"
 import Link from "next/link"
 import styles from "./login.module.css"
+import { useState } from "react"
+
 export default function Login() {
+  // get form data 
+  const [input, setInput] = useState({
+    email : "",
+    password : "",
+   }); 
+
+   // handle input change 
+   const handleInputChange = (e) => {
+      setInput((prevState) => ({
+        ...prevState,
+        [e.target.name] : e.target.value 
+      }))
+   };  
+
+   // form submit 
+   const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+   }   
+
+
   return (
     <>
       <div className={styles.login}>  
@@ -14,14 +38,14 @@ export default function Login() {
               </div>
               <div className={styles.form_data}>
                 <h3> Sign In </h3>
-                <form action="">
+                <form onSubmit={handleFormSubmit}>
                   <div className={styles.my_2}>
                     <label htmlFor="email"> Email </label>
-                    <input type="email" id="email"/>
+                    <input type="email" id="email" name="email" value={input.email} onChange={handleInputChange} />
                   </div>
                   <div className={styles.my_2}>
                     <label htmlFor="pass"> Password </label>
-                    <input type="text" id="pass"/>
+                    <input type="text" id="pass" name="password" value={input.password} onChange={handleInputChange} />
                   </div>
                   <div className={styles.my_2} style={{display : "flex", justifyContent : "space-between"}}>
                     <Link href="/register"> Create Account </Link>
--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -1,10 +1,39 @@
+"use client"
 
 import logo from "@/public/logo.svg"
 
 import styles from "./register.module.css"
 import Image from "next/image"
 import Link from "next/link"
+import { useState } from "react"
+
+
 export default function Register() {
+  // get form data 
+   const [input, setInput] = useState({
+    firstName : "",
+    lastName : "",
+    email : "",
+    password : "",
+    number : "",
+    country : ""
+   }); 
+
+   // handle input change 
+   const handleInputChange = (e) => {
+      setInput((prevState) => ({
+        ...prevState,
+        [e.target.name] : e.target.value 
+      }))
+   };  
+
+   // form submit 
+   const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+   }
+
+
   return (
     <>
       <div className={styles.signUp}>  
@@ -13,40 +42,36 @@ export default function Register() {
               <div className={styles.logo}>
                   <Image src={logo} alt="logo"/> 
               </div>
-              <div className={styles.form_data}>
+              <div className={styles.form_data}>                 
                 <h3> Create an Account </h3>
-                <form action="">
+                <form onSubmit={handleFormSubmit}>
                   <div className={styles.my_2}>
                     <div className={styles.name_box}> 
                       <div className={styles.first_name}>
                         <label htmlFor="fastName"> First Name </label>
-                        <input type="text" id="fastName" />
+                        <input type="text" id="fastName" name="firstName" value={input.firstName} onChange={handleInputChange}/>
                       </div>
                       <div className={styles.last_name}>
                       <label htmlFor="lastName"> Last Name </label>
-                        <input type="text" id="lastName" />
+                        <input type="text" id="lastName" name="lastName" value={input.lastName} onChange={handleInputChange}/>
                       </div>
                     </div>
                   </div>
                   <div className={styles.my_2}>
                     <label htmlFor="email"> Email </label>
-                    <input type="email" id="email"/>
+                    <input type="email" id="email" name="email" value={input.email} onChange={handleInputChange} />
                   </div>
                   <div className={styles.my_2}>
                     <label htmlFor="number"> Phone Number </label>
-                    <input type="text" id="number"/>
+                    <input type="text" id="number" name="number" value={input.number} onChange={handleInputChange}/>
                   </div>
                   <div className={styles.my_2}>
                     <label htmlFor="pass"> Password </label>
-                    <input type="text" id="pass"/>
-                  </div>
-                  <div className={styles.my_2}>
-                    <label htmlFor="company"> Company Name (legal entity) </label>
-                    <input type="text" id="company"/>
+                    <input type="text" id="pass" name="password" value={input.password} onChange={handleInputChange} />
                   </div>
                   <div className={styles.my_2}>
                     <label htmlFor="country"> Country</label>
-                    <select name="country" id="">
+                    <select name="country" value={input.country} onChange={handleInputChange}>
                        <option value="Bangladesh"> Bangladesh </option>
                        <option value="India"> India </option>
                        <option value="Sri Lanka"> Sri Lanka </option>
